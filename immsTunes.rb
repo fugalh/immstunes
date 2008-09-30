47a7ed9e64e1ba9c6ac5ae4198013e143a9ab0d5
Hans Fugal <hans@fugal.net>
2008-09-29 23:21:17 -0600

    untested dispatch code

--- immsTunes.rb
+++ immsTunes.rb
@@ -7,8 +7,28 @@ require 'logger'
 HOME = Pathname.new(ENV['HOME'])
 $log = Logger.new STDERR
 
+
+# iTunes isn't a valid class name and ITunes is tacky.
+# TODO think through this API - how much syntactic sugar?
+class Tunes
+  include Appscript
+  # The music library
+  attr :library
+
+  # The playlist we manage, e.g. Party Shuffle
+  attr :playlist
+
+  def initialize
+    @app = app('iTunes')
+    @playlist = @app.playlists[its.special_kind.eq(:Party_Shuffle)].first.get
+    @library = @app.library_playlist.first.get
+  end
+end
+
 class IMMS
   def initialize
+    @tunes = Tunes.new
+
     begin
       @sock = UNIXSocket.open(HOME + '.imms' + 'socket')
     rescue Errno::ECONNREFUSED
@@ -24,16 +44,62 @@ class IMMS
     unless version == 'Version 2.1'
       $log.warning "I'm not programmed for version #{version}" 
     end
-  end
-end
 
-class Tunes
-  include Appscript
-  def initialize
-    @app = app('iTunes')
-    @playlist = @app.playlists[its.special_kind.eq(:Party_Shuffle)].first.get
+    @sock.puts 'IMMS'
+
+    # add debug output on @sock.puts
+    class << @sock
+      def puts(str)
+        $log.debug "> #{str.strip}"
+        super
+      end
+    end
+
   end
+
+  def dispatch
+    Thread.new do
+      until @sock.eof
+        line = @sock.gets.strip
+        $log.debug "< #{line}"
+        cmd = line.split.first
+        case cmd
+        when 'ResetSelection'
+          # TODO ???
+        when 'TryAgain'
+          @sock.puts 'SelectNext'
+        when 'EnqueueNext'
+          pos = line.split.last.to_i
+          @tunes.playlist.add(@tunes.library.tracks[pos].location.get)
+        when 'PlaylistChanged'
+          $log.error line
+          len = line.split.last
+          @sock.puts "PlaylistChanged #{len}"
+        when 'GetPlaylistItem'
+          pos = line.split.last.to_i
+          path = @tunes.tracks[pos].location.get
+          @sock.puts "PlaylistItem #{pos} #{path}"
+        when 'GetEntirePlaylist'
+          @tunes.tracks.each_with_index do |track, pos|
+            path = track.location.get 
+            @sock.puts "Playlist #{pos} #{path}"
+          end
+          @sock.puts "PlaylistEnd"
+        else
+          $log.error "Unknown Command #{cmd}"
+        end
+      end
+    end
+  end
+
+  def control
+    current_track = nil
+    fin = false
+    loop do
+      sleep 1
+    end
+  end
+
 end
 
 imms = IMMS.new
-tunes = Tunes.new
