548753a9373025f6356befe530e55c2b31e9d281
Hans Fugal <hans@fugal.net>
2008-09-30 12:43:11 -0600

    growl with cute image

--- growl_imms.rb
+++ growl_imms.rb
@@ -1,5 +1,6 @@
 #! /usr/bin/env ruby
 require 'pathname'
+Image = Pathname.new 'm.png'
 HOME = Pathname.new(ENV['HOME'])
 IO.popen("tail -f -q -n 1 #{HOME + '.imms' + 'imms.log'}", 'r') { |f|
   state = 0
@@ -8,7 +9,7 @@ IO.popen("tail -f -q -n 1 #{HOME + '.imms' + 'imms.log'}", 'r') { |f|
     l.strip!
     if l =~ /Rating: ([\d()]+).*After: ([\d()]+)/
       msg = "Before: #{$1}, After: #{$2}"
-      system 'growlnotify', '-m', msg, '-d', 'imms.log', '-p', '-2', '-n', 'IMMS'
+      system 'growlnotify', '-m', msg, '-d', 'imms.log', '-p', '-2', '-n', 'IMMS', '--image', Image.to_s
       state = 0
     end
   end
